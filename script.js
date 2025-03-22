9746712e1709a75e4fcb679de88f740d9576f46d
Aaanika-Islam <nusratanika214@gmail.com>
2025-03-22 10:15:42 +0600

    product

---
+++ script.js
@@ -0,0 +1,9 @@
+// "Add to Cart" button function
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+// "View Details" button function
+function viewDetails(productName) {
+    alert("More details about " + productName + " are coming soon!");
+}
