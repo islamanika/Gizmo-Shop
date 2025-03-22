166e22b982ad249c3593d99104f5c23bbf4af599
Aaanika-Islam <nusratanika214@gmail.com>
2025-03-22 11:00:48 +0600

    new commit

---
+++ script-product.js
@@ -0,0 +1,9 @@
+// Function to handle "Add to Cart" button click
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+// Function to handle "View Details" button click
+function viewDetails(productName) {
+    alert("More details about " + productName + " are coming soon!");
+}
