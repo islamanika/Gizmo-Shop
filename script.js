bb207a3fb60dba0358e8371639b7c9989fa1fa7f
Aaanika-Islam <nusratanika214@gmail.com>
2025-03-22 11:15:03 +0600

    updatee

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 
-
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".add-to-cart");
 
     buttons.forEach(button => {
-        button.removeEventListener("click", handleAddToCart); // Remove existing event listener if any
-        button.addEventListener("click", handleAddToCart);
+        button.addEventListener("click", function () {
+            alert("Item added to cart!");
+        });
     });
 });
 
-// Function to handle Add to Cart button
-function handleAddToCart(event) {
-    const productName = event.target.closest(".product").querySelector("h3").innerText;
-    addToCart(productName);
-}
 
-// "Shop Now" button function 
-function shopNow() {
-    alert("Redirecting to the products page");
-    window.location.href = "products.html";
-}
 
-// "Add to Cart" button function
-function addToCart(productName) {
-    alert(productName + " has been added to your cart!");
-}
-
-// "View Details" button function
-function viewDetails(productName) {
-    alert("More details about " + productName + " are coming soon!");
-}
